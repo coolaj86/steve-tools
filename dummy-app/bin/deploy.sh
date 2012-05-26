932e91c7558db0e7cacfeeae33a1c627416b1aae
AJ ONeal <coolaj86@gmail.com>
2012-05-25 23:22:03 -0600

    shim for deploy to run before publish

--- dummy-app/bin/deploy.sh
+++ dummy-app/bin/deploy.sh
@@ -12,19 +12,25 @@ popd
 
 pushd browser
   WEBPUB='../webclient-deployed'
-  rm -rf "${WEBPUB}"
-  mkdir -p "${WEBPUB}/"
 
+  echo -n "Killing off old cruft..."
+  rm -rf "${WEBPUB}"
+
+  echo -n "Copying in static files..."
+  mkdir -p "${WEBPUB}/"
   rsync -a static/ "${WEBPUB}/"
   # make sure there's always a favicon, even if it's broken
   touch "${WEBPUB}/favicon.ico"
 
-  jade index.jade
-  mv index.html "${WEBPUB}/"
+  echo -n "Compiling Jade to HTML..."
+  jade *.jade
+  mv *.html "${WEBPUB}/"
 
+  echo -n "Compiling LESS to CSS and minifying..."
   lessc style.less > style.css
   mv style.css "${WEBPUB}/"
 
+  echo -n "Compiling JavaScript to CommonJS..."
   pakmanager build
   rm pakmanaged.html
   uglifyjs pakmanaged.js > pakmanaged.min.js
